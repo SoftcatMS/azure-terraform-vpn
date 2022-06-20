2f91a46fcf29e26c4290b3c7293cb7cc122ce796
Nielsen Pierce <Nielsenpi@softcat.com>
2022-06-20 14:21:38 +0100

    Update VPN module

--- tests/advanced/main.tf
+++ tests/advanced/main.tf
@@ -1,58 +1,97 @@
-resource "azurerm_resource_group" "rg-vpn-test-advanced" {
-  name     = "rg-test-vpn-advanced-resources"
-  location = "UK South"
-}
+# resource "azurerm_resource_group" "rg-vpn-test-advanced" {
+#   name     = "rg-test-vpn-advanced-resources"
+#   location = "UK South"
+# }
 
 
-module "vnet" {
-  source              = "github.com/SoftcatMS/azure-terraform-vnet"
-  vnet_name           = "vnet-test-vpn-advanced"
-  resource_group_name = azurerm_resource_group.rg-vpn-test-advanced.name
-  address_space       = ["10.3.0.0/16"]
-  subnet_prefixes     = ["10.3.1.0/24", "10.3.2.0/26"]
-  subnet_names        = ["subnet1", "GatewaySubnet"]
+# module "vnet1" {
+#   source              = "github.com/SoftcatMS/azure-terraform-vnet"
+#   vnet_name           = "vnet1-test-vpn1-advanced"
+#   resource_group_name = azurerm_resource_group.rg-vpn-test-advanced.name
+#   address_space       = ["10.3.0.0/16"]
+#   subnet_prefixes     = ["10.3.1.0/26"]
+#   subnet_names        = ["GatewaySubnet"]
 
-  tags = {
-    environment = "test"
-    engineer    = "ci/cd"
-  }
+#   tags = {
+#     environment = "test"
+#     engineer    = "ci/cd"
+#   }
 
-  depends_on = [azurerm_resource_group.rg-vpn-test-advanced]
-}
+#   depends_on = [azurerm_resource_group.rg-vpn-test-advanced]
+# }
 
-module "simple" {
-  source = "../../"
+# module "vnet2" {
+#   source              = "github.com/SoftcatMS/azure-terraform-vnet"
+#   vnet_name           = "vnet2-test-vpn2-advanced"
+#   resource_group_name = azurerm_resource_group.rg-vpn-test-advanced.name
+#   address_space       = ["10.4.0.0/16"]
+#   subnet_prefixes     = ["10.4.1.0/26"]
+#   subnet_names        = ["GatewaySubnet"]
 
-  name                = "vpn-test-advanced"
-  resource_group_name = azurerm_resource_group.rg-vpn-test-advanced.name
-  location            = azurerm_resource_group.rg-vpn-test-advanced.location
-  subnet_id           = module.vnet.vnet_subnets[1]
-  sku                 = "VpnGw1AZ"
+#   tags = {
+#     environment = "test"
+#     engineer    = "ci/cd"
+#   }
 
-  tags = {
-    environment = "test"
-    engineer    = "ci/cd"
-  }
+#   depends_on = [azurerm_resource_group.rg-vpn-test-advanced]
 
-  local_networks = [
-    {
-      name            = "onpremise"
-      gateway_address = "8.8.8.8"
-      address_space = [
-        "10.0.0.0/8"
-      ]
-      shared_key = "TESTING"
-      ipsec_policy = {
-        dh_group         = "DHGroup2"
-        ike_encryption   = "AES256"
-        ike_integrity    = "SHA256"
-        ipsec_encryption = "AES256"
-        ipsec_integrity  = "SHA256"
-        pfs_group        = "PFS24"
-        sa_datasize      = "102400000"
-        sa_lifetime      = "3600"
-      }
-    },
-  ]
+# }
 
-}
+# module "advanced1" {
+#   source = "../../"
+
+#   resource_group_name  = azurerm_resource_group.rg-vpn-test-advanced.name
+#   location             = azurerm_resource_group.rg-vpn-test-advanced.location
+#   virtual_network_name = module.vnet1.vnet_name
+#   vpn_gateway_name     = "vpn1-test-advanced-gw01"
+#   gateway_type         = "Vpn"
+
+
+#   # local network gateway connection
+#   local_networks = [
+#     {
+#       local_gw_name         = "onpremise"
+#       local_gateway_address = "8.8.8.8"
+#       local_address_space   = ["10.4.0.0/24"]
+#       shared_key            = "aSh4redS3kret!"
+#     },
+#   ]
+
+
+#   tags = {
+#     environment = "test"
+#     engineer    = "ci/cd"
+#   }
+
+
+# }
+
+
+# module "advanced2" {
+#   source = "../../"
+
+#   resource_group_name  = azurerm_resource_group.rg-vpn-test-advanced.name
+#   location             = azurerm_resource_group.rg-vpn-test-advanced.location
+#   virtual_network_name = module.vnet2.vnet_name
+#   vpn_gateway_name     = "vpn2-test-advanced-gw01"
+#   gateway_type         = "Vpn"
+
+
+#   # local network gateway connection
+#   local_networks = [
+#     {
+#       local_gw_name         = "onpremise"
+#       local_gateway_address = "8.8.8.9"
+#       local_address_space   = ["10.3.0.0/24"]
+#       shared_key            = "aSh4redS3kret!"
+#     },
+#   ]
+
+
+#   tags = {
+#     environment = "test"
+#     engineer    = "ci/cd"
+#   }
+
+
+# }
